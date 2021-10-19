635d714ab94853c97db8cb0d5e74dfe660cf2ff3
Meyra <75731320+SuMeyraG@users.noreply.github.com>
2021-10-19 16:13:28 +0300

    some bugs about extra features

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -7,28 +7,41 @@ public class PlayerControl : MonoBehaviour
 
     public bool isPlayer1;
     Rigidbody2D rb2d;
-    
+
     public float speed;
 
     LevelController levelController;
 
+    bool hasAlreadyDownSize;
+    float startDowningTime;
+    public float downSizeDuration = 5;
+
     public int score;
+
     void Start()
     {
-      rb2d = GetComponent<Rigidbody2D>();
-      levelController = GameObject.FindGameObjectWithTag("LevelController").GetComponent<LevelController>();
+        rb2d = GetComponent<Rigidbody2D>();
+        levelController = GameObject.FindGameObjectWithTag("LevelController").GetComponent<LevelController>();
     }
 
     public void ResetPositionPlayer1()
     {
         transform.position = new Vector3(0, -4.4f, 0);
-    }
+        //if (isPlayer1)
+        //{
+        //    transform.position = new Vector3(0, -4.4f, 0);
+        //}
+        //else
+        //{
+        //    transform.position = new Vector3(0, 4.4f, 0);
+        //}
 
+        //transform.position = new Vector3(0, 4.4f * (isPlayer1 ? -1 : 1), 0);
+    }
     public void ResetPositionPlayer2()
     {
         transform.position = new Vector3(0, 4.4f, 0);
     }
-
     void Update()
     {
         if (isPlayer1)
@@ -37,7 +50,51 @@ public class PlayerControl : MonoBehaviour
         }
         else
         {
-            rb2d.velocity = Vector2.right * Input.GetAxisRaw("Horizontal2") * speed; 
+            rb2d.velocity = Vector2.right * Input.GetAxisRaw("Horizontal2") * speed;
+        }
+
+        if (!hasAlreadyDownSize)
+        {
+            if (!isPlayer1 && Input.GetKeyDown(KeyCode.Q)) //Pressed by Player2
+            {
+                DownSize(isPlayer1);
+            }
+
+            if (isPlayer1 && Input.GetKeyDown(KeyCode.L))  //Pressed by Player1
+            {
+                DownSize(!isPlayer1);
+            }
+        }
+
+        if (hasAlreadyDownSize && startDowningTime + 5 < Time.time)
+        {
+            UpSize(isPlayer1);
         }
     }
+
+
+    void DownSize(bool isPlayer1)
+    {
+        startDowningTime = Time.time;
+
+        if (isPlayer1)
+        {
+            transform.localScale = transform.localScale / 2;
+            Debug.Log("Küçüldüm");
+        }
+
+        hasAlreadyDownSize = true;
+    }
+
+    void UpSize(bool isPlayer1)
+    {
+        if (isPlayer1)
+        {
+            transform.localScale = transform.localScale * 2;
+        }
+
+
+        hasAlreadyDownSize = false;
+    }
+
 }
